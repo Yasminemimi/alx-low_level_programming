3585ad6a32022c7f2476fdf12bcdb110e3866a62
root <juhjumine23@gmail.com>
2023-04-22 18:39:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "Uppercase then lowercase followed by a new line"
+ * Return: always 0
+ */
+int main(void)
+{
+int n = 97;
+int m = 65;
+while (n <= 122)
+{
+putchar(n);
+}
+while (m <= 90)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
